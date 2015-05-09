9b57e620645e1e25d6325da352469f5a567c287b
lbraglia <lbraglia@gmail.com>
2015-05-10 01:29:27 +0200

    better testing

--- tests/testthat/test_wrong_fc.R
+++ tests/testthat/test_wrong_fc.R
@@ -1,11 +1,18 @@
 context("wrong_fc tests")
 
 test_that("Regular fiscal codes", {
-            expect_true(wrong_fc("qWeASd34D12h 221M   "))
+            ## right
             expect_false(wrong_fc("QWEASD34D12H221M"))
+            ## wrong
+            expect_true(wrong_fc("qWeASd34D12h 221M   "))
+            expect_true(wrong_fc("QWEASD34D12H221X"))
+            expect_true(wrong_fc("QWEASD34D12H221m"))
           })
 
 test_that("Temporary fiscal codes", {
-            expect_true(wrong_fc(" 12312312312 "))
+            ## right
             expect_false(wrong_fc("12312312312"))
+            ## wrong
+            expect_true(wrong_fc(" 12312312312 "))
+            expect_true(wrong_fc("12312312315"))
           })
